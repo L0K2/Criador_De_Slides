8eabb604598219e3ea4adc0be9bf3eae9f637b27
Rafael Soster <70975828+L0K2@users.noreply.github.com>
2023-10-19 23:01:09 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# Arquivo de configuração para a geração dos slides
+
+from pptx.dml.color import RGBColor
+
+SLIDE_PADRAO = 'Padrao'
+COR_DA_FONTE = RGBColor(0xFF, 0xFF, 0xFF) #branco
+FONTE_LETRAS = 'K2D SemiBold'
+FONTE_TITULO = 'K2D ExtraBold'
+PASTA_SAIDA = 'FINAL'
+
+
+
+""" CONFIG PARA GERAÇÂO
+FontNameSM = "K2D SemiBold" #Fonte letras
+FontNameST = "K2D ExtraBold" #Fonte titulo e subtitulo
+SLIDE_PADRAO = 'Padrao'
+COR_DA_FONTE = RGBColor(0xFF, 0xFF, 0xFF) #branco
+FONTE_LETRAS = 'K2D SemiBold'
+FONTE_TITULO = 'K2D ExtraBold'
+PASTA_SAIDA = 'FINAL'
+"""
